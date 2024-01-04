263ee6f12cce1fce459451e52e045898e1ab84bb
wenyg <yg_wen@126.com>
2024-01-04 15:56:11 +0800

    update

--- blog.sh
+++ blog.sh
@@ -5,16 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CONTAINER_BLOG_DIR="/blog"
 case "$1" in
-	# 生成 html 资源文件到 nginx-deploy/html 目录
-	gen)
-		docker run -it --rm \
-			-v ${SCRIPT_DIR}/source:${CONTAINER_BLOG_DIR}/source \
-			-v ${SCRIPT_DIR}/themes/even:${CONTAINER_BLOG_DIR}/themes/even \
-			-v ${SCRIPT_DIR}/_config.yml:${CONTAINER_BLOG_DIR}/_config.yml \
-			-v ${SCRIPT_DIR}/nginx-deploy/html/:${CONTAINER_BLOG_DIR}/public/ \
-			hexo /bin/bash -lic "hexo g"
-			#hexo /bin/bash -lic "hexo g && cp -fr public/* /html/"
-		;;
 	serve)
 		docker run -it --rm \
 			--net=host \
