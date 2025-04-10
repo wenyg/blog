203a38f951ac6cbe0ae3574f5d4cd3fd65ab2ad1
wenyg <yg_wen@126.com>
2025-04-10 18:01:09 +0800

    update

--- blog.sh
+++ blog.sh
@@ -7,7 +7,7 @@ CONTAINER_BLOG_DIR="/blog"
 NGINX_DIR=${SCRIPT_DIR}/../nginx-deploy/nginx/
 case "$1" in
 	serve)
-		docker run -it --rm \
+		docker run -it --rm -d \
 			--net=host \
 			-v ${SCRIPT_DIR}/source:${CONTAINER_BLOG_DIR}/source \
 			-v ${SCRIPT_DIR}/themes/even:${CONTAINER_BLOG_DIR}/themes/even \
@@ -17,7 +17,7 @@ case "$1" in
 	# 启动 nginx
 
 	run)
-		docker run --rm  --net=host \
+		docker run --rm  -d --net=host \
 			--mount type=bind,source=${NGINX_DIR}/nginx.conf,target=/etc/nginx/nginx.conf \
 			-v ${NGINX_DIR}/ssl:/etc/nginx/ssl \
 			-v ${NGINX_DIR}/d:/d \
---
+++ source/_posts/tools/install_node.md
@@ -0,0 +1,14 @@
+---
+title: "ubuntu20.04 react开发"
+tags: [react, node]
+categories: 工具 
+---
+
+```
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+node -v
+sudo corepack enable
+sudo npm install -g yarn
+yarn -v
+```
